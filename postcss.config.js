f030090e3c90b0cb5efd765a6e706e092a08d1cf
mari-hollanda <mariannahollanda@gmail.com>
2025-05-18 14:34:54 -0300

    Setting up the react and tailwind

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // Use o novo nome do pacote aqui
+    autoprefixer: {}, // Mantenha outros plugins como autoprefixer se estiver usando
+  },
+};
